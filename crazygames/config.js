2800ecdef08a77e7decbdcc3435195da3529fdff
jayeshkolipc1-afk <jayeshkolipc1@gmail.com>
2026-09-16 11:54:59 +0530

    Add multiplayer client config

---
+++ crazygames/config.js
@@ -0,0 +1,13 @@
+/*
+ WARZONE: LAST STAND
+ Production multiplayer endpoint.
+ Priority:
+  1) ?server=wss://...
+  2) this value
+  3) localhost for local testing only
+*/
+window.WLS_CONFIG = {
+  multiplayerServerUrl:
+    new URLSearchParams(location.search).get("server") ||
+    "wss://YOUR-RENDER-SERVICE.onrender.com"
+};
